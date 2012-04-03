eed47ce59770acb577692e5bc8664d0501275143
Pat O'Keefe <patokeefe1@gmail.com>
2012-04-02 23:55:16 -0400

    Queue now supports keywords.

--- updater.py
+++ updater.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python
 
 """
-craigsuck, a Craigslist RSS poller.
+Craigslist RSS poller.
 Copyright (c) 2011. Jake Brukhman <jbrukh@gmail.com>. See LICENSE. 
+Modified April 2012 by Pat O'Keefe <patokeefe1@gmail.com>
 """
 
 import craigslist
 import optparse
 import time
 import sys 
+import smtplib
+import conf
 from string import Template
 
 class LookupQueue(object):
-    """
-    A bounded queue backed by a set for fast
-    membership lookup and which does not accept
+    """A bounded queue backed by a set for fast membership lookup and which does not accept
     duplicate elements.
+
+    4/2012 -- Acceptable posts now need to contain a keyword from the configuration
+    file.
     """
 
     def __init__(self, size):
@@ -26,11 +30,22 @@ class LookupQueue(object):
     def push(self, *items):
         for item in items:
             if item not in self.s:
-                if len(self.q) == self.size:
-                    self.pop()
-                self.s.add(item)
-                self.q.append(item)
-                return item
+                if self.containsKeyword(item):
+                    if len(self.q) == self.size:
+                        self.pop()
+                    self.s.add(item)
+                    self.q.append(item)
+                    return item
+
+
+    def containsKeyword(self, item):
+        postTitle = item['title'].lower()
+        for k in conf.KEYWORDS:
+            if k in postTitle:
+                # print "Pushed to queue because ", postTitle, " contains ", k
+                return True
+        return False
+
         
     def pop(self):
         item = self.q.pop(0)
@@ -49,6 +64,9 @@ class LookupQueue(object):
     def __repr__(self):
         return self.q.__str__()
 
+
+
+
 def main(query, opts):
     """
     Indefinitely cycles through the queries provided to the program,
@@ -58,10 +76,24 @@ def main(query, opts):
     while True:
         listings = craigslist.fetch_with_pages_back(query, pages=opts.pages)
         new_listings = [l for l in listings if queue.push(l['link'])]
-        for listing in new_listings:
-            print Template(opts.format).safe_substitute(listing)
+
+        # # If you just want to print out the relevant posts...
+        # for listing in new_listings:
+        #     print Template(opts.format).safe_substitute(listing)
+
+        # Attempt to send an email containing the new posts
+        if len(new_listings) > 0:
+            msg = get_msg(new_listings, query)
+            try:
+                send_email(conf.SENDER, conf.RECIPIENTS.split(';'), msg)
+            except:
+                print "Could not send email: ", sys.exc_info()[0]
+
         process_new(new_listings)
         time.sleep(opts.sleep)
+
+
+
 
 def process_new(listings):
     pass
